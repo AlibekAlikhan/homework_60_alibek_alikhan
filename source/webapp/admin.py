9cf15b36060d2c999da74b83280308d954adefc5
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-18 23:31:31 +0600

    изменил миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -5,10 +5,10 @@ from webapp.models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("id", "status", "category", "create_at")
-    list_filter = ("id", "status", "category", "create_at")
+    list_display = ("id", "status", "price", "create_at")
+    list_filter = ("id", "status", "price", "create_at")
     search_fields = ("status", "text")
-    filter = ("text", "status", "category", "create_at")
+    filter = ("text", "status", "price", "create_at")
     readonly_fields = ("id", "create_at")
 
 
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,23 +2,10 @@ from django.db import models
 
 
 # Create your models here.
-class Category(models.Model):
-    status_category = models.CharField(max_length=10, null=False, verbose_name="Статус")
-    text_category = models.TextField(max_length=3000, null=True, verbose_name="Текст")
 
-    def str(self):
-        return f"{self.status_category} - {self.text_category}"
 
 
 class Article(models.Model):
-    category = models.ForeignKey(
-        to='webapp.Category',
-        verbose_name='Категории',
-        null=False,
-        blank=False,
-        related_name='articles',
-        on_delete=models.RESTRICT
-    )
     status = models.CharField(max_length=10, null=False, verbose_name="Статус")
     text = models.TextField(max_length=3000, null=True, verbose_name="Текст")
     price = models.DecimalField(max_digits=15, decimal_places=0, default=0, verbose_name="Цена")
@@ -29,3 +16,18 @@ class Article(models.Model):
 
     def str(self):
         return f"{self.status} - {self.text}"
+
+    class Category(models.Model):
+        article = models.ForeignKey(
+            to='webapp.Article',
+            verbose_name='Продукты',
+            null=False,
+            blank=False,
+            related_name='categories',
+            on_delete=models.RESTRICT
+        )
+        status_category = models.CharField(max_length=10, null=False, verbose_name="Статус")
+        text_category = models.TextField(max_length=3000, null=True, verbose_name="Текст")
+
+        def str(self):
+            return f"{self.status_category} - {self.text_category}"
