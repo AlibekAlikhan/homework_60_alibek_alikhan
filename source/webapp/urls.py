13ecf2d9dc396a142c90a0deef49a4372472df79
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-20 18:27:39 +0600

    изменения в сервире

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from webapp.views.articles import article_view, article_create, detail_view
+from webapp.views.articles import product_view, product_create, detail_view, category_create, category_view
 
 
 urlpatterns =[
-    path('', article_view, name="index_article"),
-    path('article', article_view, name="index_article"),
-    path('article/create', article_create, name="create_article"),
-    path('article/<int:pk>', detail_view, name="detail_view")
+    path('', product_view, name="index_article"),
+    path('article', product_view, name="index_article"),
+    path('article/create', product_create, name="create_article"),
+    path('article/<int:pk>', detail_view, name="detail_view"),
+    path('article/create_category', category_create, name="create_category"),
+    path('article/category', category_view, name="category_view")
 ]
--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,10 +1,10 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect, get_object_or_404
 
-from webapp.models import Article
+from webapp.models import Article, Category
 
 
-def article_view(request: WSGIRequest):
+def product_view(request: WSGIRequest):
     articles = Article.objects.all()
     context = {
         "articles": articles
@@ -12,17 +12,43 @@ def article_view(request: WSGIRequest):
     return render(request, "tasks.html", context=context)
 
 
-def article_create(request: WSGIRequest):
+def category_view(request: WSGIRequest):
+    category = Category.objects.all()
+    context = {
+        "category": category
+    }
+    return render(request, "categoty.html", context=context)
+
+
+def product_create(request: WSGIRequest):
+    category = Category.objects.all()
     if request.method == "GET":
-        return render(request, "article_create.html")
-    print(request.POST)
+        context = {
+            "category": category
+        }
+        return render(request, "product_create.html", context=context)
     article_data = {
-        "status": request.POST.get('status'),
+        "name": request.POST.get('name'),
         "text": request.POST.get('text'),
-        "create_at": request.POST.get('create_at')
+        "create_at": request.POST.get('create_at'),
+        "category": request.POST.get('category'),
+        "price": request.POST.get('price'),
+        "image_url": request.POST.get('image_url')
     }
     article = Article.objects.create(**article_data)
     return redirect("detail_view", pk=article.pk)
+
+
+def category_create(request: WSGIRequest):
+    if request.method == "GET":
+        return render(request, "categoty_create.html")
+    print(request.POST)
+    categoty_data = {
+        "status_category": request.POST.get('status_category'),
+        "text_category": request.POST.get('text_category'),
+    }
+    Category.objects.create(**categoty_data)
+    return redirect("category_view")
 
 
 def detail_view(request: WSGIRequest, pk):
