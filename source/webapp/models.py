2118e0b7763ad67d3bb74ee45a4456c8b6b4f717
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-28 15:13:57 +0600

    Сделал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,28 +1,23 @@
 from django.db import models
+from django.db.models import TextChoices
 
 
 # Create your models here.
 
-class Category(models.Model):
-    status_category = models.CharField(max_length=10, null=False, verbose_name="Статус")
-    text_category = models.TextField(max_length=3000, null=True, verbose_name="Текст")
-
-    def str(self):
-        return f"{self.status_category} - {self.text_category}"
+class StatusChoice(TextChoices):
+    OTHER = 'other', 'разное'
+    TECHNIQUE = 'technique', 'техника'
+    CLOTH = 'cloth', 'одежда'
+    TOYS = 'toys', 'игрушки'
 
 
 class Article(models.Model):
-    category = models.ForeignKey(
-        to='webapp.Category',
-        verbose_name='категории',
-        null=False,
-        blank=False,
-        related_name='articles',
-        on_delete=models.RESTRICT
-    )
-    name = models.CharField(max_length=10, null=False, verbose_name="Статус")
-    text = models.TextField(max_length=3000, null=True, verbose_name="Текст")
-    price = models.DecimalField(max_digits=15, decimal_places=0, default=0, verbose_name="Цена")
+    category = models.CharField(verbose_name="Категория", choices=StatusChoice.choices, max_length=20,
+                                default=StatusChoice.OTHER)
+    name = models.CharField(max_length=100, null=False, verbose_name="Имя")
+    text = models.TextField(max_length=2000, null=True, verbose_name="Текст")
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0, verbose_name="Цена")
+    remainder = models.IntegerField(verbose_name="Остаток")
     image_url = models.TextField(max_length=3000, null=True, verbose_name="Фото")
     create_at = models.DateField(auto_now_add=True, verbose_name="Дата добавления")
 
