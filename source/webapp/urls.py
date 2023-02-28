75fb5bca2f712f4c1635d2e1808809988aab8f18
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-28 15:16:29 +0600

    Добавил фенкционалы удаления, изменения

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views.articles import product_view, product_create, detail_view, category_create, category_view
+from webapp.views.articles import product_view, product_create, detail_view, product_update, delete, deleted_confirm
 
 
 urlpatterns =[
@@ -8,6 +8,7 @@ urlpatterns =[
     path('article', product_view, name="index_article"),
     path('article/create', product_create, name="create_article"),
     path('article/<int:pk>', detail_view, name="detail_view"),
-    path('article/create_category', category_create, name="create_category"),
-    path('article/category/<int:pk>', category_view, name="category_view")
+    path('article/<int:pk>/update', product_update, name="product_update"),
+    path('article/<int:pk>/delit', delete, name="article_delit"),
+    path('article/<int:pk>/delit/confirm', deleted_confirm, name="confirm")
 ]
--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,7 +1,9 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect, get_object_or_404
 
-from webapp.models import Article, Category
+from webapp.models import Article
+
+from webapp.forms import ArticleForm
 
 
 def product_view(request: WSGIRequest):
@@ -12,43 +14,16 @@ def product_view(request: WSGIRequest):
     return render(request, "tasks.html", context=context)
 
 
-def category_view(request: WSGIRequest, pk):
-    category = get_object_or_404(Category, pk=pk)
-    context = {
-        "category": category
-    }
-    return render(request, "categoty.html", context=context)
-
-
 def product_create(request: WSGIRequest):
-    category = Category.objects.all()
     if request.method == "GET":
-        context = {
-            "category": category
-        }
-        return render(request, "product_create.html", context=context)
-    article_data = {
-        "name": request.POST.get('name'),
-        "text": request.POST.get('text'),
-        "create_at": request.POST.get('create_at'),
-        "category": get_object_or_404(Category, pk=int(request.POST.get('category'))),
-        "price": request.POST.get('price'),
-        "image_url": request.POST.get('image_url')
-    }
-    article = Article.objects.create(**article_data)
-    return redirect("detail_view", pk=article.pk)
-
-
-def category_create(request: WSGIRequest):
-    if request.method == "GET":
-        return render(request, "categoty_create.html")
-    print(request.POST)
-    categoty_data = {
-        "status_category": request.POST.get('status_category'),
-        "text_category": request.POST.get('text_category'),
-    }
-    category = Category.objects.create(**categoty_data)
-    return redirect("category_view", pk=category.pk)
+        form = ArticleForm()
+        return render(request, "product_create.html", context={"form": form})
+    form = ArticleForm(data=request.POST)
+    if not form.is_valid():
+        return render(request, "product_create.html", context={"form": form})
+    else:
+        article = Article.objects.create(**form.cleaned_data)
+        return redirect("detail_view", pk=article.pk)
 
 
 def detail_view(request: WSGIRequest, pk):
@@ -56,3 +31,28 @@ def detail_view(request: WSGIRequest, pk):
     return render(request, "detail_article.html", context={
         'article': article
     })
+
+
+def product_update(request: WSGIRequest, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("detail_view", pk=article.pk)
+        return render(request, "product_update.html", context={"form": form, article: article})
+    form = ArticleForm(instance=article)
+    return render(request, "product_update.html", context={"form": form, article: article})
+
+
+def delete(request: WSGIRequest, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, "delete_confirm.html", context={
+        'article': article
+    })
+
+
+def deleted_confirm(request: WSGIRequest, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect("index_article")
